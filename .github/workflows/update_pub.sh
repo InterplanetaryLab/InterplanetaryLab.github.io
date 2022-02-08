354e9974a3b492de4526998ad5575c21368163d9
Rommac100 <rommac100@gmail.com>
2022-02-08 10:20:45 -0700

    trying new custom github actions

---
+++ .github/workflows/update_pub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Removing previous published documents area
+head -n 9 _pages/published-documents.md > tmp.txt
+
+# appending new readme file content
+wget https://raw.githubusercontent.com/InterplanetaryLab/communications/main/README.md -O readme2
+tail -n +4 readme2 >> tmp.txt
+rm -r readme2
+mv tmp.txt _pages/published-documents.md
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add .
+git commit -am "updated Publications page"
+git push

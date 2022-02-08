70dcb640f2e3a549a7efe3c6d7fe9096311033be
Rommac100 <rommac100@gmail.com>
2022-02-08 10:42:33 -0700

    more testing

--- .github/workflows/update_pub.sh
+++ .github/workflows/update_pub.sh
@@ -7,8 +7,8 @@ wget https://raw.githubusercontent.com/InterplanetaryLab/communications/main/REA
 tail -n +4 readme2 >> tmp.txt
 rm -r readme2
 mv tmp.txt _pages/published-documents.md
-git config user.name github-actions
-git config user.email github-actions@github.com
+git remote set-url origin ${{ secrets.ssh }}
 git add .
 git commit -am "updated Publications page"
+git fetch origin master
 git push origin HEAD:master
